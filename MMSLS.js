ce95da1d64b29e50003de9e106cda74f9ffd26a5
dxd110 <77091129+dxd110@users.noreply.github.com>
2024-03-08 00:06:50 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=24921597&pid=278b5e384c1ac290a5b3fe558e21ddfc&tid=a27e62d72e6d0a1a5fe1c0bc356eba85",
+    "https://www.maimemo.com/share/page?uid=24921597&pid=71a93b75a4a016ad4ef6b8243b89d7a1&tid=998086a969541818e9155ae5414f15c3",
 
 
   ]
